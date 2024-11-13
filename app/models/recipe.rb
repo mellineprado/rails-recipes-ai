8877c494ce961a8711ab2674f3a2fd3b45569aeb
melline prado <mellinevprado@hotmail.com>
2024-11-13 11:57:31 -0300

    refactored recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -13,15 +13,21 @@ class Recipe < ApplicationRecord
 
   def set_content
     client = OpenAI::Client.new
-    chaptgpt_response = client.chat(parameters: {
-      model: "gpt-3.5-turbo",
-      messages: [{
-        role: "user",
-        content: "Give me a simple recipe for #{name} with the ingredients #{ingredients}. Give me only the text of the recipe, without any of your own answer like 'Here is a simple recipe'."}]
-    })
-    new_content = chaptgpt_response["choices"][0]["message"]["content"]
+    chaptgpt_response = client.chat(
+      parameters: {
+        model: "gpt-3.5-turbo",
+        messages: [{
+          role: "user",
+          content: "Give me a simple recipe for #{name} with the ingredients #{ingredients}. " \
+                   "Give me only the text of the recipe, without any of your own answer like " \
+                   "'Here is a simple recipe'."
+        }]
+      }
+    )
 
+    new_content = chaptgpt_response["choices"][0]["message"]["content"]
     update(content: new_content)
-    return new_content
+
+    new_content
   end
 end
